459ad2945cd05e91d3a15894fcfef2b8fe7ce0f8
matt <matt@lotfi.se>
2016-11-02 12:43:58 +0100

    Made classes that implements interface

---
+++ src/prop/assignment0/Parser.java
@@ -0,0 +1,23 @@
+package prop.assignment0;
+
+import java.io.IOException;
+
+/**
+ * Created by matt on 2016-11-02.
+ */
+public class Parser implements IParser {
+    @Override
+    public void open(String fileName) throws IOException, TokenizerException {
+
+    }
+
+    @Override
+    public INode parse() throws IOException, TokenizerException, ParserException {
+        return null;
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+}
---
+++ src/prop/assignment0/Tokenizer.java
@@ -0,0 +1,28 @@
+package prop.assignment0;
+
+import java.io.IOException;
+
+/**
+ * Created by matt on 2016-11-02.
+ */
+public class Tokenizer implements ITokenizer {
+    @Override
+    public void open(String fileName) throws IOException, TokenizerException {
+
+    }
+
+    @Override
+    public Lexeme current() {
+        return null;
+    }
+
+    @Override
+    public void moveNext() throws IOException, TokenizerException {
+
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+}
