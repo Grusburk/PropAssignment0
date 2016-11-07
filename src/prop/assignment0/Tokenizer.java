50f920a52dca3ea5bb5afb9f9120eee23be81646
matt <matt@lotfi.se>
2016-11-07 14:43:23 +0100

    Add Lexeme to Toknizer

--- src/prop/assignment0/Tokenizer.java
+++ src/prop/assignment0/Tokenizer.java
@@ -31,32 +31,60 @@ public class Tokenizer implements ITokenizer {
     public void moveNext() throws IOException, TokenizerException {
 		removeWhiteSpace();
 		char inputChar = scanner.current();
-		switch (inputChar){
-			case 'a':
-//				current = produceDeterminer();
-				break;
-			case '1':
-			case '2':
-			case '3':
-			case '4':
-			case '5':
-			case '6':
-			case '7':
-			case '8':
-			case '9':
-			case '0':
-				// bla bla bla
-				break;
-			default:
+		if (inputChar >= 'a' && inputChar <='z') {
+			current = produceIdentifier(inputChar);
+		}else if(inputChar >= '0' && inputChar <='9'){
+			current = produceInt(inputChar);
+		}else{
+			switch (inputChar) {
+				case '+':
+					current = produceAddOp(inputChar);
+					break;
+				case '=':
+					current = produceAssign(inputChar);
+					break;
+				case '-':
+					current = produceSubOp(inputChar);
+					break;
+				case '*':
+					current = produceMultOp(inputChar);
+					break;
+				case '/':
+					current = produceDivOp(inputChar);
+					break;
+				default:
+					break;
+			}
 		}
     }
-	
-//	private char produceDeterminer(){
-//		if (char != 'a'){
-//			return null;
-//		}
-//		return inputChar;
-//	}
+
+	private Lexeme produceInt(char inputChar) {
+		return new Lexeme(0,Token.INT_LIT);
+	}
+
+	private Lexeme produceAddOp(char inputChar) {
+		return new Lexeme(0,Token.ADD_OP);
+	}
+
+	private Lexeme produceMultOp(char inputChar) {
+		return new Lexeme(0,Token.MULT_OP);
+	}
+
+	private Lexeme produceSubOp(char inputChar) {
+		return new Lexeme(0,Token.SUB_OP);
+	}
+
+	private Lexeme produceDivOp(char inputChar) {
+		return new Lexeme(0,Token.DIV_OP);
+	}
+
+	private Lexeme produceIdentifier(char inputChar) {
+		return new Lexeme(0,Token.IDENT);
+	}
+
+	private Lexeme produceAssign(char input){
+		return new Lexeme(0,Token.ASSIGN_OP);
+	}
 
     @Override
     public void close() throws IOException {
