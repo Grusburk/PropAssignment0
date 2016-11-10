43ac0010a7207882b19a024d516f63373eaba27b
Mattin Lotfi <matt@lotfi.se>
2016-11-10 16:28:21 +0100

    Reworked Stringbuilder in Node

--- src/prop/assignment0/FactorNode.java
+++ src/prop/assignment0/FactorNode.java
@@ -6,7 +6,35 @@
 public class FactorNode implements INode {
     private FactorNode leftNode;
     private FactorNode rightNode;
-    private ExprNode value;
+    public Lexeme lexeme;
+
+    public FactorNode(Lexeme current) {
+
+    }
+
+    public FactorNode getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(FactorNode leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public FactorNode getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(FactorNode rightNode) {
+        this.rightNode = rightNode;
+    }
+
+    public Lexeme getLexeme() {
+        return lexeme;
+    }
+
+    public void setLexeme(Lexeme lexeme) {
+        this.lexeme = lexeme;
+    }
 
     @Override
     public Object evaluate(Object[] args) throws Exception {
@@ -15,6 +43,19 @@ public class FactorNode implements INode {
 
     @Override
     public void buildString(StringBuilder builder, int tabs) {
+        builder.append("FactorNode\n");
+//        System.out.println(rightNode.lexeme);
+        builder.append(leftNode.lexeme.token() + " " + leftNode.lexeme.value() + "\n");
+        builder.append(rightNode.lexeme.token() + " " + rightNode.lexeme.value() + "\n");
+
+
+//        rightNode.buildString(builder, tabs);
+//        leftNode.buildString(builder, tabs);
+//        tabs++;
+//        builder.append(leftNode.lexeme);
+//        tabs--;
+//        rightNode.buildString(builder, tabs);
+//        builder.append(rightNode.lexeme);
 
     }
 }
