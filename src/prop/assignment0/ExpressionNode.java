832956207878cfcf9c8f4a07c5066d5054a3d8cc
Mattin Lotfi <matt@lotfi.se>
2016-11-12 15:01:51 +0100

    Builder for ExpressionNode working

--- src/prop/assignment0/ExpressionNode.java
+++ src/prop/assignment0/ExpressionNode.java
@@ -3,6 +3,7 @@ package prop.assignment0;
 /**
  * Created by matt on 2016-11-02.
  */
+
 public class ExpressionNode implements INode {
     private INode termNode;
     private INode exprNode;
@@ -12,10 +13,6 @@ public class ExpressionNode implements INode {
 
     }
 
-//    public ExpressionNode(Lexeme current) {
-//        lexeme = current;
-//    }
-
     @Override
     public Object evaluate(Object[] args) throws Exception {
         return lexeme.value();
@@ -23,31 +20,36 @@ public class ExpressionNode implements INode {
 
     @Override
     public void buildString(StringBuilder builder, int tabs) {
+        appendTab(builder, tabs);
         builder.append("ExpressionNode\n");
-//        leftNode.buildString(builder, tabs);
-//        builder.append(lexeme.token() + " " + lexeme.value()  + "\n");
-//        builder.append(rightNode.lexeme.token() + " " + rightNode.lexeme.value() + "\n");
+        tabs++;
+        if (termNode != null){
+            termNode.buildString(builder, tabs);
+        }
+        if (lexeme != null){
+            appendTab(builder, tabs);
+            builder.append(lexeme + "\n");
+            if (exprNode != null){
+                exprNode.buildString(builder, tabs);
+            }
+        }
     }
 
-//    public TermNode getLeftNode() {
-//        return leftNode;
-//    }
+    private void appendTab (StringBuilder builder, int tabs) {
+        for(int i = 0; i < tabs; i++){
+            builder.append("\t");
+        }
+    }
 
     public void setTermNode(INode termNode) {
         this.termNode = termNode;
     }
 
-//    public ExpressionNode getRightNode() {
-//        return rightNode;
-//    }
 
     public void setExprNode(INode exprNode) {
         this.exprNode = exprNode;
     }
 
-//    public Lexeme getLexeme() {
-//        return lexeme;
-//    }
 
     public void setLexeme(Lexeme lexeme) {
         this.lexeme = lexeme;
