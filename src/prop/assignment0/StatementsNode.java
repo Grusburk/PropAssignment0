025d9e6182232ce2c9cb956ad1e3c29aecef5c28
Mattin Lotfi <matt@lotfi.se>
2016-11-14 14:54:25 +0100

    StatementNode Stringbuilder Working

---
+++ src/prop/assignment0/StatementsNode.java
@@ -0,0 +1,41 @@
+    package prop.assignment0;
+
+/**
+ * Created by matt on 2016-11-02.
+ */
+public class StatementsNode implements INode {
+
+    private INode assignNode, stmtsNode;
+
+    @Override
+    public Object evaluate(Object[] args) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void buildString(StringBuilder builder, int tabs) {
+        appendTab(builder, tabs);
+        builder.append("StatementsNode\n");
+        tabs++;
+        if (assignNode != null){
+            assignNode.buildString(builder, tabs);
+        }
+        if (stmtsNode != null){
+            stmtsNode.buildString(builder, tabs);
+        }
+    }
+
+    private void appendTab (StringBuilder builder, int tabs) {
+        for(int i = 0; i < tabs; i++){
+            builder.append("\t");
+        }
+    }
+
+    public void setAssignNode(INode assignNode){
+        this.assignNode = assignNode;
+    }
+
+    public void setStmtsNode(INode stmtsNode){
+        this.stmtsNode = stmtsNode;
+    }
+}
